00d2fc75be9edfba6deb1e82cec27e187741a3f3
Jan Vansteenkiste <jan@vstone.eu>
2022-01-25 11:54:25 +0100

    Do not use top scope variable facts

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@
 # It sets variables according to platform
 #
 class mdadm::params {
-  case $::osfamily {
+  case $facts['osfamily'] {
     'Debian': {
       $package_name = 'mdadm'
       $package_ensure = 'present'
@@ -13,7 +13,8 @@ class mdadm::params {
       # This means Debian jessie (mdadm 3.3.2)/stretch (mdadm 3.4) and Ubuntu
       # bionic (mdadm 4.0) for instance use 'mdmonitor' as the service name,
       # but Ubuntu xenial (mdadm 3.3) and below still use 'mdadm'
-      if ($operatingsystem == 'Ubuntu' and versioncmp($operatingsystemrelease, '16.10') < 0) or ($operatingsystem == 'Debian' and versioncmp($operatingsystemrelease, '8') < 0) {
+      if ($facts['operatingsystem'] == 'Ubuntu' and versioncmp($facts['operatingsystemrelease'], '16.10') < 0) or
+        ($facts['operatingsystem'] == 'Debian' and versioncmp($facts['operatingsystemrelease'], '8') < 0) {
         $service_name = 'mdadm'
       } else {
         $service_name = 'mdmonitor'
@@ -23,7 +24,7 @@ class mdadm::params {
       $service_manage = true
 
       # Older mdadm packages don't have a service status
-      if versioncmp($operatingsystemrelease, '12.04') < 0 {
+      if versioncmp($facts['operatingsystemrelease'], '12.04') < 0 {
         $service_hasstatus = false
       } else {
         $service_hasstatus = true
@@ -32,7 +33,7 @@ class mdadm::params {
       $include_cron = true
     }
     default: {
-      fail("${::operatingsystem} not supported")
+      fail("${facts['operatingsystem']} not supported")
     }
   }
 }
