5505d5f0393e6b49ce27f2309abd83b1245907ee
Jan Vansteenkiste <jan@vstone.eu>
2022-01-25 11:55:29 +0100

    Convert tests to use on_supported_os for facts
    The services specs have been skipped since they need additional logic
    with versioning

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -1,23 +1,20 @@
 require 'spec_helper'
 
 describe 'mdadm', :type => :class do
-  describe 'mdadm::config class on Debian' do
-    let(:facts) {{
-      :osfamily => 'Debian',
-      :operatingsystem => 'Ubuntu',
-      :operatingsystemrelease => '12.04',
-    }}
+  on_supported_os.each do |os, facts|
+    describe "mdadm::config class on #{os}" do
+      let(:facts) { facts }
+      context 'with no parameters' do
+        it { should contain_file('/etc/cron.d/mdadm') }
+      end
 
-    context 'with no parameters' do
-      it { should contain_file('/etc/cron.d/mdadm') }
-    end
+      context 'with parameters' do
+        let(:params) {{
+          :include_cron => false,
+        }}
 
-    context 'with parameters' do
-      let(:params) {{
-        :include_cron => false,
-      }}
-
-      it { should_not contain_file('/etc/cron.d/mdadm') }
+        it { should_not contain_file('/etc/cron.d/mdadm') }
+      end
     end
   end
 end
--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -1,28 +1,26 @@
 require 'spec_helper'
 
 describe 'mdadm', :type => :class do
-  describe 'mdadm::config class on Debian' do
-    # Default facts used for contexts
-    let(:facts) {{
-      :osfamily => 'Debian',
-      :operatingsystem => 'Ubuntu',
-      :operatingsystemrelease => '12.04',
-    }}
+  on_supported_os.each do |os, facts|
+    describe "mdadm::install class on #{os}" do
+      # Default facts used for contexts
+      let(:facts) { facts }
 
-    context 'with no parameters' do
-      it { should contain_package('mdadm') }
-    end
+      context 'with no parameters' do
+        it { should contain_package('mdadm') }
+      end
 
-    context 'with parameters' do
-      let(:params) {{
-        :package_name => 'raidtools',
-        :package_ensure => '1.2.3',
-      }}
+      context 'with parameters' do
+        let(:params) {{
+          :package_name => 'raidtools',
+          :package_ensure => '1.2.3',
+        }}
 
-      it { should contain_package('raidtools').with({
-        'ensure' => '1.2.3'
+        it { should contain_package('raidtools').with({
+          'ensure' => '1.2.3'
         })
-      }
+        }
+      end
     end
   end
 end
